3728e6eb4a7acef2810314ef9b71e688330e537f
fortunato <sambataro1990@gmail.com>
2020-11-18 14:12:04 +0100

    funziona ma senza caratteri speciali

---
+++ palindromo/js/script.js
@@ -0,0 +1,24 @@
+// Palindroma
+// Chiedere all’utente di inserire una parola.
+// Creare una funzione per capire se la parola inserita è palindroma.
+
+
+// Chiedere all’utente di inserire una parola.
+var parolaUtente = prompt("inserisci una parola");
+
+
+// Creare una funzione per capire se la parola inserita è palindroma.
+function palindromo(){
+// come controllo se la parola è un palindromo?
+var controlloPalindromo = parolaUtente.split("").reverse().join("");
+
+// utilizzo il controllo
+if(parolaUtente == controlloPalindromo){
+
+  document.getElementById('stampa').innerHTML= parolaUtente + " è un palindromo";
+}else{
+  document.getElementById('stampa').innerHTML= parolaUtente + " non è un palindromo";
+}
+}
+// utilizzo la funzione
+palindromo(parolaUtente)
