78d1949c10a31431470ecd18921efefd639171a2
fortunato <sambataro1990@gmail.com>
2020-11-18 16:14:32 +0100

    esercizio completato
    voglio aggiungere controllo in caso di pareggio e in caso di selezione  incongruente pari/dispari numero da parte dell'utente

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -5,38 +5,42 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
 // Dichiariamo chi ha vinto.
 
-
+// chiedo all'utente pari o dispari
 var pariDispari = (prompt("scegli  pari o dispari"));
-
-
 console.log(pariDispari);
-
+// chiedo all'utente un numero
 var numeroUtente = parseInt(prompt("scegli un numero da 1 a 5"));
-
 console.log(numeroUtente);
 
-
+// creo funzione numero casuale del computer
 function numeroRandom() {
   numeroComputer = Math.floor(Math.random() * 6);
 }
 
 numeroRandom();
-
 console.log(numeroComputer);
 
+// sommo il numero computer al numero utente
 var numeroFinale = numeroUtente + numeroComputer;
-
 console.log(numeroFinale);
 
+
+// stabilisco il vincitore
 function pariOdispari() {
 
   if(numeroFinale % 2 == 0) {
     console.log("pari");
 }
-
 else {
-    console.log("dispari.");
+    console.log("dispari");
 }
 }
 
 pariOdispari() ;
+
+// stampo il vincitore
+if (numeroFinale == pariDispari) {
+  console.log("hai vinto");
+} else {
+  console.log("hai perso");
+}
