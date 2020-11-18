afcaac748a66cb80c5d96f44d81ad73a3da80b30
fortunato <sambataro1990@gmail.com>
2020-11-18 15:26:48 +0100

    esercizio palindromo completato

--- palindromo/js/script.js
+++ palindromo/js/script.js
@@ -4,22 +4,40 @@
 
 
 // Chiedere all’utente di inserire una parola.
-var parolaUtente = prompt("inserisci una parola");
+var stampata = document.getElementById("stampa")
+var buttonAnnulla = document.getElementById("annulla");
 
+
+
+var buttonGenera = document.getElementById('genera');
+buttonGenera.addEventListener("click",
 
 // Creare una funzione per capire se la parola inserita è palindroma.
 function palindromo(){
 // come controllo se la parola è un palindromo?
+// fix caratteri speciali e maiuscole
+var parolaUtente = document.getElementById("parola").value;
 var fixCaratteriSpeciali = parolaUtente.replace(/[^A-Z0-9]/ig, "").toLowerCase();
-var controlloPalindromo = parolaUtente.split("").reverse().join("");
+var controlloPalindromo = fixCaratteriSpeciali.split("").reverse().join("");
 
 // utilizzo il controllo
-if(parolaUtente == controlloPalindromo){
+if(fixCaratteriSpeciali == controlloPalindromo){
 
   document.getElementById('stampa').innerHTML= parolaUtente + " è un palindromo";
 }else{
   document.getElementById('stampa').innerHTML= parolaUtente + " non è un palindromo";
 }
+stampata.className = "show";
 }
 // utilizzo la funzione
-palindromo(parolaUtente)
+);
+
+// annullo la funzione
+buttonAnnulla.addEventListener("click",
+function() {
+
+  // pulizia  valori
+  stampata.className = "hidden";
+  document.getElementById("stampa").value = "";
+}
+);
